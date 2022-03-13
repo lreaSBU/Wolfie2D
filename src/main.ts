9a128d7c94ddeb1ee1bf9e689ca5bf0b1f21f989
ZGrandison <73369239+ZGrandison@users.noreply.github.com>
2022-03-13 11:48:17 -0400

    hw5 push

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Game from "./Wolfie2D/Loop/Game";
-import default_scene from "./default_scene";
+import MainMenu from "./Homework5/Scenes/MainMenu";
 
 // The main function is your entrypoint into Wolfie2D. Specify your first scene and any options here.
 (function main(){
@@ -9,14 +9,22 @@ import default_scene from "./default_scene";
     // Set up options for our game
     let options = {
         canvasSize: {x: 1200, y: 800},          // The size of the game
-        clearColor: {r: 0, g: 0, b: 0},   // The color the game clears to
+        clearColor: {r: 34, g: 32, b: 52},   // The color the game clears to
+        inputs: [
+            {name: "left", keys: ["a"]},
+            {name: "right", keys: ["d"]},
+            {name: "jump", keys: ["w", "space"]},
+            {name: "run", keys: ["shift"]}
+        ],
+        useWebGL: false,                        // Tell the game we want to use webgl
+        showDebug: false                       // Whether to show debug messages. You can change this to true if you want
     }
 
     // Create a game with the options specified
     const game = new Game(options);
 
     // Start our game
-    game.start(default_scene, {});
+    game.start(MainMenu, {});
 })();
 
 function runTests(){};
